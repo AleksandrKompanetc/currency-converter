824c5339f6f6fdfc133f262ba8e2b27257fad5f1
AleksandrKompanetc <moneymacker7@gmail.com>
2025-01-04 19:40:42 +0200

    adding fetch to API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
@@ -6,6 +6,22 @@ function App() {
   const [amount, setAmount] = useState(1);
   const [fromCurrency, setFromCurrency] = useState('USD');
   const [toCurrency, setToCurrency] = useState('UAH');
+
+  useEffect(() => {
+    const fetchCurrencies = async () => {
+      try {
+        const response = await fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json');
+        const data = await response.json();
+
+        setCurrencies(data);
+        
+      } catch (error) {
+        console.error('Loading data error: ', error)
+      }
+    }
+
+    fetchCurrencies()
+  }, [])
 
   return (
     <div style={{textAlign: 'center', marginTop: '50px'}}>
